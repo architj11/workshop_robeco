799931fd87b9d710368b6dcf684068800fe0a7e1
architj11 <archit.j110@gmail.com>
2025-01-20 16:17:27 +0100

    python

---
+++ robeco_a.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    c = a+b
+    return c
+
+print(add(10,15))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
